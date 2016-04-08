3a18d0f1ae1e90d8c93130c4d750e7627f0e4cd4
Toshio Ito <debug.ito@gmail.com>
2016-04-08 18:16:16 +0900

    add gtk-3.0/src/output_png.pl to generate PNG assets from SVG source.

---
+++ gtk-3.0/src/output_png.pl
@@ -0,0 +1,38 @@
+use strict;
+use warnings;
+
+sub convert_svg {
+    my ($in_svg, $out_svg, $conversion) = @_;
+    open my $in_file, "<", $in_svg or die "Cannot open $in_svg: $!";
+    my $in_data = do { local $/; <$in_file> };
+    close $in_file;
+    
+    my $out_data = $conversion->($in_data);
+    open my $out_file, ">", $out_svg or die "Cannot open $out_svg: $!";
+    print $out_file $out_data;
+    close $out_file;
+}
+
+
+my %color_for = (
+    '' => '#000000',
+    '-selected' => '#FFFFFF',
+    '-insensitive' => '#999999'
+);
+
+my $TEMP_SVG = "temp.svg";
+
+foreach my $shape ('checked', 'mixed') {
+    foreach my $occasion (keys %color_for) {
+        my $in_svg = "menuitem-$shape.svg";
+        my $out_png = "menuitem-checkbox-$shape$occasion.png";
+        my $out_color = $color_for{$occasion};
+        convert_svg($in_svg, $TEMP_SVG, sub {
+            my ($data) = @_;
+            $data =~ s/stroke:#000000;/stroke:$out_color;/;
+            return $data;
+        });
+        system('inkscape', "--export-png=$out_png", "--export-area-page", "--export-width=16", "--export-height=16", $TEMP_SVG);
+    }
+}
+unlink($TEMP_SVG);
